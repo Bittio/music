cdd25747a2a23500d04d3068cba330b1501ea828
Bittio <teo.langi@edu.tampere.fi>
2019-11-13 15:40:51 +0200

    13.11.2019

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import MyNavBar from "./components/MyNavBar"
+import Karu_selli from "./components/Karu_selli"
+import images from "./components/ImageData"
+const navLinks = ["Tampere", "Helsinki", "Pori"]
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
       </header>
+    
+    <nav>
+      <MyNavBar links={navLinks}/>
+    </nav>
+    <section>
+    <Karu_selli images={images}/>
+    </section>
     </div>
   );
 }
---
+++ src/components/ImageData.js
@@ -0,0 +1,21 @@
+const images = [
+    {
+        url: "https://www.gigantti.fi/image/dv_web_D180001002185688/12836/asus-chromebook-c523-156-hd-kannettava-tietokone-hopeamusta.jpg?$prod_all4one$",
+        title: "Kannettava tietokone",
+        description: "Tässä näemme kannettavan tietokoneen luonnollisessa ympäristössään",
+        id: 0
+    },
+    {
+        url: "https://assetsds.cdnedge.bluemix.net/sites/default/files/styles/big_2/public/feature/images/potato_pc.jpg?itok=yvuy1BJ3&c=28f1b46be9014c033f7c5d34e1d03eb9",
+        title: "Tietokone",
+        description: "Tässä näemme koulun tietokoneen luonnollisessa ympäristössään",
+        id: 1
+    },
+    {
+        url: "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2Fda967aca-71e9-11e7-93f8-c74b9ea57adf.jpg?crop=2667%2C1500%2C0%2C0&resize=685",
+        title: "Emoji movie",
+        description: ":D",
+        id: 2
+    }   
+]
+export default images;
